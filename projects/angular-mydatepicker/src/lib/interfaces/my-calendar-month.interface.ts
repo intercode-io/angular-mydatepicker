e6fcd6d1cd42dfc1969fd0e43a4bac0c88cb9d98
Valik <valik.stets@gmail.com>
2019-11-22 12:34:17 +0200

    Month model changed to support dates

--- projects/angular-mydatepicker/src/lib/interfaces/my-calendar-month.interface.ts
+++ projects/angular-mydatepicker/src/lib/interfaces/my-calendar-month.interface.ts
@@ -1,7 +1,11 @@
+import { IMyDate } from './my-date.interface';
+
 export interface IMyCalendarMonth {
   nbr: number;
   name: string;
   currMonth: boolean;
   selected: boolean;
   disabled: boolean;
+  firstDate?: IMyDate;
+  lastDate?: IMyDate;
 }
